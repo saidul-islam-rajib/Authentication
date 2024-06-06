650bd946017012c32b0ba7348e0987aa11e17f22
Saidul Islam Rajib <saidul.bd@asa-international.com>
2024-06-06 20:50:02 +0600

    Completed authentication service

---
+++ Sober/Sober.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,60 @@
+﻿using ErrorOr;
+using MapsterMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Sober.Application.Authentication.Commands.Register;
+using Sober.Application.Authentication.Common;
+using Sober.Application.Authentication.Queries.Login;
+using Sober.Contracts.Authentication;
+using Sober.Domain.Common.Errors;
+
+namespace Sober.Api.Controllers
+{
+    [Route("auth")]
+    [AllowAnonymous]
+    public class AuthenticationController : ApiController
+    {
+        private readonly ISender _mediator;
+        private readonly IMapper _mapper;
+
+        public AuthenticationController(
+            ISender mediator,
+            IMapper mapper = null)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterRequest request)
+        {
+            var command = _mapper.Map<RegisterCommand>(request);
+            ErrorOr<AuthenticationResult> authResult = await _mediator.Send(command);
+
+            return authResult.Match(
+                authResult => Ok(_mapper.Map<AuthenticationResponse>(authResult)),
+                errors => Problem(errors)
+            );
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequest request)
+        {
+            var query = _mapper.Map<LoginQuery>(request);
+            var authResult = await _mediator.Send(query);
+
+            if (authResult.IsError && authResult.FirstError == Errors.Authentication.InvalidCredentials)
+            {
+                return Problem(statusCode: StatusCodes.Status401Unauthorized,
+                               title: authResult.FirstError.Description);
+            }
+
+            return authResult.Match(
+                authResult => Ok(_mapper.Map<AuthenticationResponse>(authResult)),
+                errors => Problem(errors)
+            );
+        }
+
+    }
+}
---
+++ Sober/Sober.Api/Program.cs
@@ -0,0 +1,32 @@
+using Sober.Api;
+using Sober.Application;
+using Sober.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services
+        .AddPresentation()
+        .AddApplication()
+        .AddInfrastructure(builder.Configuration);
+}
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+{
+    app.UseExceptionHandler("/error");
+    app.UseHttpsRedirection();
+    app.UseAuthentication();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
+}
