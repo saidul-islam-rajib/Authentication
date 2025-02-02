090bb245522dfab5fdcb68d27d2def8cc2cd80cd
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2025-02-02 22:45:16 +0600

    remove incaching codes

--- Sober/Sober.Infrastructure/Persistence/Repositories/UserRepository.cs
+++ Sober/Sober.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -1,15 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Authentication.Application.Common.Interfaces.Persistence;
 using Authentication.Domain.Entities.User;
-using Authentication.Infrastructure.Persistence;
 
 namespace Authentication.Infrastructure.Persistence.Repositories;
 
 public class UserRepository : IUserRepository
 {
-    private readonly PortfolioDbContext _context;
-
-    private static readonly List<User> _users = new();
+    private readonly PortfolioDbContext _context;    
     private readonly IPasswordHasher<User> _passwordHasher;
 
     public UserRepository(PortfolioDbContext context, IPasswordHasher<User> passwordHasher)
@@ -20,14 +17,13 @@ public class UserRepository : IUserRepository
 
     public void Add(User user)
     {
-        _users.Add(user);
         _context.Add(user);
         _context.SaveChanges();
     }
 
     public User? GetUserByEmail(string email)
     {
-        return _users.SingleOrDefault(u => u.Email == email);
+        return _context.Users.SingleOrDefault(u => u.Email == email);
     }
 
     public async Task<bool> VerifyPasswordAsync(User user, string password)
