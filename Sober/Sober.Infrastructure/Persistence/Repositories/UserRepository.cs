c5161e5f897428edb958b5eec04eadcc5da62e36
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2025-02-02 21:42:34 +0600

    Add users into db

--- Sober/Sober.Infrastructure/Persistence/Repositories/UserRepository.cs
+++ Sober/Sober.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -5,10 +5,20 @@ namespace Sober.Infrastructure.Persistence.Repositories
 {
     public class UserRepository : IUserRepository
     {
+        private readonly PortfolioDbContext _context;
+
         private static readonly List<User> _users = new();
+
+        public UserRepository(PortfolioDbContext context)
+        {
+            _context = context;
+        }
+
         public void Add(User user)
         {
             _users.Add(user);
+            _context.Add(user);
+            _context.SaveChanges();
         }
 
         public User? GetUserByEmail(string email)
